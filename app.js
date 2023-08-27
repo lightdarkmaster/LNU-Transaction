83831bbedf075d9397cbaec04ddeec1eec3d9e28
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-08-27 22:30:56 +0800

    Add Show password

---
+++ app.js
@@ -0,0 +1,12 @@
+function togglePasswordVisibility() {
+    const passwordField = document.getElementById("password");
+    const toggleBtn = document.querySelector(".toggle-password");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        toggleBtn.textContent = "Hide";
+    } else {
+        passwordField.type = "password";
+        toggleBtn.textContent = "Show";
+    }
+}
